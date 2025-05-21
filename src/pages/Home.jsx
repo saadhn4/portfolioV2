c0750e741f47e67651a7cdd0299d5f4f48237df2
saadhn4 <saadcfi4@gmail.com>
2025-05-21 14:50:35 +0400

    changed hero pic

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,7 +12,6 @@ import goat from "../assets/goat2.jpeg";
 
 import { Link } from "react-router-dom";
 
-
 const Home = () => {
   return (
     <>
@@ -24,7 +23,7 @@ const Home = () => {
       >
         <div className="left">
           <img
-            src={heroPic}
+            src="https://sdmntprsouthcentralus.oaiusercontent.com/files/00000000-26a0-61f7-91c7-9ba1ea8074e2/raw?se=2025-05-21T11%3A49%3A21Z&sp=r&sv=2024-08-04&sr=b&scid=f33b4be7-51db-51f1-a5c3-8ead5cf85103&skoid=30ec2761-8f41-44db-b282-7a0f8809659b&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2025-05-20T22%3A52%3A57Z&ske=2025-05-21T22%3A52%3A57Z&sks=b&skv=2024-08-04&sig=1SVXyn0Nsk4HSlvu1m/V25DQId4z96J5G2ebllA/gmA%3D"
             alt="hero-pic"
             className="w-60 h-60 rounded-2xl"
           />
@@ -47,7 +46,10 @@ const Home = () => {
             <a href="https://github.com/saadhn4" target="_blank">
               <i className="bi bi-github text-2xl"></i>
             </a>
-            <a href="https://www.linkedin.com/in/saadhussain04/" target="_blank">
+            <a
+              href="https://www.linkedin.com/in/saadhussain04/"
+              target="_blank"
+            >
               <i className="bi bi-linkedin text-2xl"></i>
             </a>
             <a href="https://www.instagram.com/saad.hn/" target="_blank">
@@ -88,10 +90,30 @@ const Home = () => {
       >
         <h1 className="text-3xl mb-8 font-semibold">Projects</h1>
         <div className="flex flex-wrap justify-center gap-x-20 gap-y-5">
-          <ProjectCard picture={eatSure} title="EatSure" desc="Restaurant Review Application built with MERN stack." link="https://github.com/saadhn4/capstone-fr" />
-          <ProjectCard picture={gitFind} title="GitHub Finder" desc="Search GitHub user profiles using the GitHub REST API." link="https://github-finder-topaz.vercel.app/" />
-          <ProjectCard picture={twitterAppPic} title="Twitter Clone" desc="A simple Twitter-style mini posting app built using React." link="https://saad-twitter-clone.vercel.app//" />
-          <ProjectCard picture={goat} title="Cat Generator" desc="Fun React app that fetches and displays a random cat image using the Cataas API." link="https://cat-api-phi.vercel.app/" />
+          <ProjectCard
+            picture={eatSure}
+            title="EatSure"
+            desc="Restaurant Review Application built with MERN stack."
+            link="https://github.com/saadhn4/capstone-fr"
+          />
+          <ProjectCard
+            picture={gitFind}
+            title="GitHub Finder"
+            desc="Search GitHub user profiles using the GitHub REST API."
+            link="https://github-finder-topaz.vercel.app/"
+          />
+          <ProjectCard
+            picture={twitterAppPic}
+            title="Twitter Clone"
+            desc="A simple Twitter-style mini posting app built using React."
+            link="https://saad-twitter-clone.vercel.app//"
+          />
+          <ProjectCard
+            picture={goat}
+            title="Cat Generator"
+            desc="Fun React app that fetches and displays a random cat image using the Cataas API."
+            link="https://cat-api-phi.vercel.app/"
+          />
         </div>
         <div className="flex justify-center items-center mt-4">
           <Link to="/projects">More</Link>
